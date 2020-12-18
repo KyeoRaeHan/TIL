61ab437dd91d99a154c0f31ac4f666eee755b030
Ray.Han <59241047+KyeoRaeHan@users.noreply.github.com>
2020-12-18 23:01:00 +0900

    Add files via upload

---
+++ Study_Java/ArrayEx01.java
@@ -0,0 +1,25 @@
+package arrayex;
+
+public class ArrayEx01 {
+
+	public static void main(String[] args) {
+		int[] stuffs = {2000, 3000, 5000, 7000, 2500, 8000, 9000, 1000, 3500, 8300};
+		double avg;
+		
+		System.out.println("상품가격 Lists");
+		System.out.println("--------------------------------");
+		
+		
+//		for(int i=0; i<stuffs.length; i++) {
+//			
+//			System.out.println("살품[" + i + "] : "  + stuffs[i]);
+//			
+//		}
+//		
+		for(int stuff : stuffs) {
+			System.out.println("살품[" + i + "] : " stuff);
+		
+	}
+
+	}
+}
